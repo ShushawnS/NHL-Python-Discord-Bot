d337116c7da48777dce728be33761fcf8eec0a47
spezza19 <shailen.chander@me.com>
2021-07-06 22:46:10 -0400

    cleaning up code into functions file
    put functions into the functions file instead of crowding the main.py

---
+++ functions.py
@@ -0,0 +1,202 @@
+import os
+import discord 
+import asyncio
+from dotenv import load_dotenv
+from discord.ext import commands 
+import requests   
+import json 
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+
+#Get Roster Function 
+def getRoster(ctx,team):
+    print("OI0EUWHTFOEIUWHRIUEFHRYWIOUEHFIOUEHWWOHIUWEIOUOHIUIH")
+    #Creates and Runs API Request [Roster Details]
+    base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/roster'
+    url = base_url.format(team)
+    response = requests.get(url)
+    #print(response.status_code)
+    #print(response.text) 
+
+    #Makes JSON Information A Python Object
+    rosterJSON = response.text 
+    rosterData = json.loads(rosterJSON)   
+
+    #Creates and Runs API Request [Team Stats Details]
+    base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/stats'
+    url = base_url.format(team)
+    response = requests.get(url)
+    #print(response.status_code)
+    #print(response.text)
+
+    #Makes JSON Information A Python Object
+    nhlJSON = response.text
+    nhlData = json.loads(nhlJSON)
+
+    #Create Embed 
+    myEmbed = discord.Embed(title = 'Roster Details For: ', description = f"{nhlData['stats'][0]['splits'][0]['team']['name']} \n \n", color = 0x00ff00)            
+    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url) 
+    myEmbed.set_footer(text = "NHL BOT -- CREATED BY: SHUSHAWN & SHAILEN") 
+    myEmbed.set_thumbnail(url="https://th.bing.com/th/id/R8b66c2b4984717a8844c22fcce69cd57?rik=5k2K87pjvH5CQQ&pid=ImgRaw")
+
+    #Finds Length of Object and Adds Embed Field [Each ]
+    length = len(rosterData['roster'])
+    x = 0
+    for x in range(length):
+        myEmbed.add_field(name = f"**{rosterData['roster'][x]['person']['fullName']} **", value = f" \n > Position: {rosterData['roster'][x]['position']['name']} [{rosterData['roster'][x]['position']['code']}] \n > Jersey Number: {rosterData['roster'][x]['jerseyNumber']} \n > ID: {rosterData['roster'][x]['person']['id']} ", inline = True)
+    
+    return myEmbed
+    #ctx.send(embed = myEmbed) 
+
+#Gets Player ID
+def getPlayerID(ctx, name):
+    print(name)
+    playerName = name
+    #Creates and Runs API Request [Player Details]
+    base_url = 'https://suggest.svc.nhl.com/svc/suggest/v1/minplayers/{}'
+    url = base_url.format(playerName)
+    response = requests.get(url)
+    #print(response.status_code)
+    #print(response.text) 
+
+    #Makes JSON Information A Python Object
+    nhlJSON = response.text
+    nhlData = json.loads(nhlJSON) 
+
+    print("Please give me player id \n\n") 
+    nhlData = nhlData['suggestions'][0].split('|')
+
+    print(nhlData[0])
+    return(nhlData[0])
+
+#Gets Player Function
+def getPlayer(ctx, id):
+    #Creates and Runs API Request [Player Details]
+    base_url = 'https://statsapi.web.nhl.com/api/v1/people/{}'
+    url = base_url.format(id)
+    response = requests.get(url)
+    #print(response.status_code)
+    #print(response.text)  
+
+    #Makes JSON Information A Python Object
+    playerJSON = response.text 
+    playerData = json.loads(playerJSON)  
+    
+    leadership = ' '
+    #Adds Details If A Player Is Leader
+    if (playerData['people'][0]['active'] == 'true'):
+        if (playerData['people'][0]['captain']):
+            leadership = ' [Captain]'
+        elif (playerData['people'][0]['alternateCaptain']):
+            leadership = ' [Assistant Captain]' 
+    
+    #Creates and Runs API Request [Player Stats Details]
+    base_url = 'https://statsapi.web.nhl.com/api/v1/people/{}/stats?stats=statsSingleSeason&season=20202021'
+    url = base_url.format(id)
+    response = requests.get(url)
+    print(response.status_code) 
+    #print(response.text) 
+
+    #Makes JSON Information A Python Object
+    playerStatsJSON = response.text 
+    playerStatsData = json.loads(playerStatsJSON)
+
+    #Create Embed 
+    myEmbed = discord.Embed(title = f" {playerData['people'][0]['fullName']} {leadership} \n \n", description = f" > [NHL.com](https://www.nhl.com/player/{playerData['people'][0]['firstName']}-{playerData['people'][0]['lastName']}-{id}) | [TSN.ca](https://www.tsn.ca/nhl/player-bio/{playerData['people'][0]['firstName'].lower()}-{playerData['people'][0]['lastName'].lower()}) \n", color = 0x00ff00)            
+    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url) 
+    myEmbed.set_footer(text = "NHL BOT -- CREATED BY: SHUSHAWN & SHAILEN") 
+    myEmbed.set_thumbnail(url=f"http://nhl.bamcontent.com/images/headshots/current/168x168/{id}.jpg")
+
+    #Creates a Field For Each Piece of Information
+    myEmbed.add_field(name = "**Basic Details: **", value = f" > Age: {playerData['people'][0]['currentAge']} \n > Birth Date: {playerData['people'][0]['birthDate']} \n > Birth City: {playerData['people'][0]['birthCity']} \n > Nationality: {playerData['people'][0]['nationality']} \n > Height: {playerData['people'][0]['height']} \n > Weight: {playerData['people'][0]['weight']} ", inline = True) 
+    myEmbed.add_field(name = "**Current Team: **", value = f" > Organization: {playerData['people'][0]['currentTeam']['name']} \n > Jersey Number: {playerData['people'][0]['primaryNumber']}", inline = True)
+
+    if playerData['people'][0]['primaryPosition']['code'] != 'G' and (playerStatsData['stats'][0]['splits']):
+
+        myEmbed.add_field(name = f"**Player Stats For: ** [{playerStatsData['stats'][0]['splits'][0]['season']}]", value = f" > GP: {playerStatsData['stats'][0]['splits'][0]['stat']['games']} \n > G: {playerStatsData['stats'][0]['splits'][0]['stat']['goals']} \n > A: {playerStatsData['stats'][0]['splits'][0]['stat']['assists']} \n > TP: {playerStatsData['stats'][0]['splits'][0]['stat']['points']} \n > +/-: {playerStatsData['stats'][0]['splits'][0]['stat']['plusMinus']}  ", inline = True) 
+        
+        #myEmbed.add_field(name = "Helpful Links", value = f" [country codes](https://countrycode.org/) ", inline = True)
+    elif playerData['people'][0]['primaryPosition']['code'] == 'G' and (playerStatsData['stats'][0]['splits']):
+        myEmbed.add_field(name = f"**Player Stats For: ** [{playerStatsData['stats'][0]['splits'][0]['season']}]", value = f" > GP: {playerStatsData['stats'][0]['splits'][0]['stat']['games']} \n > W: {playerStatsData['stats'][0]['splits'][0]['stat']['wins']} \n > L: {playerStatsData['stats'][0]['splits'][0]['stat']['losses']} \n > SO: {playerStatsData['stats'][0]['splits'][0]['stat']['shutouts']} \n > GAA: {playerStatsData['stats'][0]['splits'][0]['stat']['goalAgainstAverage']} \n > SV%: {playerStatsData['stats'][0]['splits'][0]['stat']['savePercentage']} ", inline = True) 
+
+    return myEmbed
+
+def getTeamEmbed(ctx,teamID):
+    #Creates and Runs API Request [Roster Details]
+    print(f"GETTING TEAMID DATA {teamID} !!!!!\n\n\n")
+    base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/stats'
+    url = base_url.format(teamID)
+    response = requests.get(url)
+    print(response.status_code)
+    print(response.text)
+
+    #Makes JSON Information A Python Object
+    nhlJSON = response.text
+    nhlData = json.loads(nhlJSON)
+
+    #Create Embed
+    try:
+        myEmbed = discord.Embed(title=nhlData['stats'][0]['splits'][0]['team']['name'], description="")
+        myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url)
+        myEmbed.set_footer(text = f"NHL Stats - Team ID: {teamID}")
+        myEmbed.add_field(name="teamname", value=nhlData["stats"][0]["splits"][0]["team"]["name"], inline = True)
+    except:
+        myEmbed = discord.Embed(title = "Error", description="Something went wrong. This team ID probably doesn't exist or doesn't have any data associated with it.")
+        myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url)
+        myEmbed.set_footer(text = f"NHL Stats - Team ID: {teamID}")
+        #myEmbed.add_field(name="?????", value="This team ID probably doesn't exist.", inline = True)
+
+    return myEmbed
+
+'''KEEPING IN CASE BUTTONS DONT WORK WELL'''
+# async def pageReactions(ctx,embedMsg,secTimeout):
+# #given msg, watch for reactions and return -1,0,1 (⬅️❌➡️) depending on user input
+# # 0 = quit,
+
+#     def check(reaction, user):
+#     #parameters for a valid reaction
+#         print(f"user reacted with {reaction.emoji} on team embed")
+            
+#         return user == ctx.author and reaction.message.id == embedMsg.id and (reaction.emoji == "⬅️" or reaction.emoji == "➡️" or reaction.emoji == "❌")
+
+#     try:
+#         reaction, user = await ctx.bot.wait_for('reaction_add', timeout=secTimeout, check=check)
+#         return reaction
+#     except asyncio.TimeoutError:
+#         return "timeout"
+
+def deadEmbed(type):
+    if(type == "timeout"):
+        return discord.Embed(title="nice one", description="self.shit()")
+    if(type == "kill"):
+        return discord.Embed(title="adios", description="i will remember you hermano")
+
+def getSchedule(ctx):
+
+    #Creates and Runs API Request [Roster Details]
+    base_url = 'https://statsapi.web.nhl.com/api/v1/schedule'
+    #url = base_url.format(team)
+    response = requests.get(base_url)
+    print(response.status_code)
+    #print(response.text)  
+
+    #Makes JSON Information A Python Object
+    scheduleJSON = response.text 
+    scheduleData = json.loads(scheduleJSON)
+
+    totalGames = scheduleData['totalGames']
+    print(totalGames) 
+
+    #Create Embed 
+    myEmbed = discord.Embed(title = f" Schedule For: ", description = f" Today! \n", color = 0x00ff00)            
+    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url) 
+    myEmbed.set_footer(text = "NHL BOT -- CREATED BY: SHUSHAWN & SHAILEN") 
+    myEmbed.set_thumbnail(url=f"https://www-league.nhlstatic.com/images/logos/league-dark/133-flat.svg")
+
+    #Adds Details For All Games
+    x = 0
+    for x in range(totalGames):
+        myEmbed.add_field(name = f"** {scheduleData['dates'][0]['games'][x]['teams']['away']['team']['name']} vs. {scheduleData['dates'][0]['games'][x]['teams']['home']['team']['name']} **", value = f" > Arena: {scheduleData['dates'][0]['games'][x]['venue']['name']} \n > ", inline = True)
+
+
+    return myEmbed 
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import json
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
 
+import functions
+
 '''
 fixed current player error
 '''
@@ -47,203 +49,11 @@ async def on_ready():
         f'DISCORD BOT WRITTEN BY Shushawn Saha\n'
     )
 
-#Get Roster Function 
-def getRoster(ctx,team):
-    #Creates and Runs API Request [Roster Details]
-    base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/roster'
-    url = base_url.format(team)
-    response = requests.get(url)
-    #print(response.status_code)
-    #print(response.text) 
-
-    #Makes JSON Information A Python Object
-    rosterJSON = response.text 
-    rosterData = json.loads(rosterJSON)   
-
-    #Creates and Runs API Request [Team Stats Details]
-    base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/stats'
-    url = base_url.format(team)
-    response = requests.get(url)
-    #print(response.status_code)
-    #print(response.text)
-
-    #Makes JSON Information A Python Object
-    nhlJSON = response.text
-    nhlData = json.loads(nhlJSON)
-
-    #Create Embed 
-    myEmbed = discord.Embed(title = 'Roster Details For: ', description = f"{nhlData['stats'][0]['splits'][0]['team']['name']} \n \n", color = 0x00ff00)            
-    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url) 
-    myEmbed.set_footer(text = "NHL BOT -- CREATED BY: SHUSHAWN & SHAILEN") 
-    myEmbed.set_thumbnail(url="https://th.bing.com/th/id/R8b66c2b4984717a8844c22fcce69cd57?rik=5k2K87pjvH5CQQ&pid=ImgRaw")
-
-    #Finds Length of Object and Adds Embed Field [Each ]
-    length = len(rosterData['roster'])
-    x = 0
-    for x in range(length):
-        myEmbed.add_field(name = f"**{rosterData['roster'][x]['person']['fullName']} **", value = f" \n > Position: {rosterData['roster'][x]['position']['name']} [{rosterData['roster'][x]['position']['code']}] \n > Jersey Number: {rosterData['roster'][x]['jerseyNumber']} \n > ID: {rosterData['roster'][x]['person']['id']} ", inline = True)
-    
-    return myEmbed
-    #ctx.send(embed = myEmbed) 
-
-#Gets Player ID
-def getPlayerID(ctx, name):
-    print(name)
-    playerName = name
-    #Creates and Runs API Request [Player Details]
-    base_url = 'https://suggest.svc.nhl.com/svc/suggest/v1/minplayers/{}'
-    url = base_url.format(playerName)
-    response = requests.get(url)
-    #print(response.status_code)
-    #print(response.text) 
-
-    #Makes JSON Information A Python Object
-    nhlJSON = response.text
-    nhlData = json.loads(nhlJSON) 
-
-    print("Please give me player id \n\n") 
-    nhlData = nhlData['suggestions'][0].split('|')
-
-    print(nhlData[0])
-    return(nhlData[0])
-
-#Gets Player Function
-def getPlayer(ctx, id):
-    #Creates and Runs API Request [Player Details]
-    base_url = 'https://statsapi.web.nhl.com/api/v1/people/{}'
-    url = base_url.format(id)
-    response = requests.get(url)
-    #print(response.status_code)
-    #print(response.text)  
-
-    #Makes JSON Information A Python Object
-    playerJSON = response.text 
-    playerData = json.loads(playerJSON)  
-    
-    leadership = ' '
-    #Adds Details If A Player Is Leader
-    if (playerData['people'][0]['active'] == 'true'):
-        if (playerData['people'][0]['captain']):
-            leadership = ' [Captain]'
-        elif (playerData['people'][0]['alternateCaptain']):
-            leadership = ' [Assistant Captain]' 
-    
-    #Creates and Runs API Request [Player Stats Details]
-    base_url = 'https://statsapi.web.nhl.com/api/v1/people/{}/stats?stats=statsSingleSeason&season=20202021'
-    url = base_url.format(id)
-    response = requests.get(url)
-    print(response.status_code) 
-    #print(response.text) 
-
-    #Makes JSON Information A Python Object
-    playerStatsJSON = response.text 
-    playerStatsData = json.loads(playerStatsJSON)
-
-    #Create Embed 
-    myEmbed = discord.Embed(title = f" {playerData['people'][0]['fullName']} {leadership} \n \n", description = f" > [NHL.com](https://www.nhl.com/player/{playerData['people'][0]['firstName']}-{playerData['people'][0]['lastName']}-{id}) | [TSN.ca](https://www.tsn.ca/nhl/player-bio/{playerData['people'][0]['firstName'].lower()}-{playerData['people'][0]['lastName'].lower()}) \n", color = 0x00ff00)            
-    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url) 
-    myEmbed.set_footer(text = "NHL BOT -- CREATED BY: SHUSHAWN & SHAILEN") 
-    myEmbed.set_thumbnail(url=f"http://nhl.bamcontent.com/images/headshots/current/168x168/{id}.jpg")
-
-    #Creates a Field For Each Piece of Information
-    myEmbed.add_field(name = "**Basic Details: **", value = f" > Age: {playerData['people'][0]['currentAge']} \n > Birth Date: {playerData['people'][0]['birthDate']} \n > Birth City: {playerData['people'][0]['birthCity']} \n > Nationality: {playerData['people'][0]['nationality']} \n > Height: {playerData['people'][0]['height']} \n > Weight: {playerData['people'][0]['weight']} ", inline = True) 
-    myEmbed.add_field(name = "**Current Team: **", value = f" > Organization: {playerData['people'][0]['currentTeam']['name']} \n > Jersey Number: {playerData['people'][0]['primaryNumber']}", inline = True)
-
-    if playerData['people'][0]['primaryPosition']['code'] != 'G' and (playerStatsData['stats'][0]['splits']):
-
-        myEmbed.add_field(name = f"**Player Stats For: ** [{playerStatsData['stats'][0]['splits'][0]['season']}]", value = f" > GP: {playerStatsData['stats'][0]['splits'][0]['stat']['games']} \n > G: {playerStatsData['stats'][0]['splits'][0]['stat']['goals']} \n > A: {playerStatsData['stats'][0]['splits'][0]['stat']['assists']} \n > TP: {playerStatsData['stats'][0]['splits'][0]['stat']['points']} \n > +/-: {playerStatsData['stats'][0]['splits'][0]['stat']['plusMinus']}  ", inline = True) 
-        
-        #myEmbed.add_field(name = "Helpful Links", value = f" [country codes](https://countrycode.org/) ", inline = True)
-    elif playerData['people'][0]['primaryPosition']['code'] == 'G' and (playerStatsData['stats'][0]['splits']):
-        myEmbed.add_field(name = f"**Player Stats For: ** [{playerStatsData['stats'][0]['splits'][0]['season']}]", value = f" > GP: {playerStatsData['stats'][0]['splits'][0]['stat']['games']} \n > W: {playerStatsData['stats'][0]['splits'][0]['stat']['wins']} \n > L: {playerStatsData['stats'][0]['splits'][0]['stat']['losses']} \n > SO: {playerStatsData['stats'][0]['splits'][0]['stat']['shutouts']} \n > GAA: {playerStatsData['stats'][0]['splits'][0]['stat']['goalAgainstAverage']} \n > SV%: {playerStatsData['stats'][0]['splits'][0]['stat']['savePercentage']} ", inline = True) 
-
-    return myEmbed
-
-def getTeamEmbed(ctx,teamID):
-    #Creates and Runs API Request [Roster Details]
-    print(f"GETTING TEAMID DATA {teamID} !!!!!\n\n\n")
-    base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/stats'
-    url = base_url.format(teamID)
-    response = requests.get(url)
-    print(response.status_code)
-    print(response.text)
-
-    #Makes JSON Information A Python Object
-    nhlJSON = response.text
-    nhlData = json.loads(nhlJSON)
-
-    #Create Embed
-    try:
-        myEmbed = discord.Embed(title=nhlData['stats'][0]['splits'][0]['team']['name'], description="")
-        myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url)
-        myEmbed.set_footer(text = f"NHL Stats - Team ID: {teamID}")
-        myEmbed.add_field(name="teamname", value=nhlData["stats"][0]["splits"][0]["team"]["name"], inline = True)
-    except:
-        myEmbed = discord.Embed(title = "Error", description="Something went wrong. This team ID probably doesn't exist or doesn't have any data associated with it.")
-        myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url)
-        myEmbed.set_footer(text = f"NHL Stats - Team ID: {teamID}")
-        #myEmbed.add_field(name="?????", value="This team ID probably doesn't exist.", inline = True)
-
-    return myEmbed
-
-'''KEEPING IN CASE BUTTONS DONT WORK WELL'''
-# async def pageReactions(ctx,embedMsg,secTimeout):
-# #given msg, watch for reactions and return -1,0,1 (⬅️❌➡️) depending on user input
-# # 0 = quit,
-
-#     def check(reaction, user):
-#     #parameters for a valid reaction
-#         print(f"user reacted with {reaction.emoji} on team embed")
-            
-#         return user == ctx.author and reaction.message.id == embedMsg.id and (reaction.emoji == "⬅️" or reaction.emoji == "➡️" or reaction.emoji == "❌")
-
-#     try:
-#         reaction, user = await ctx.bot.wait_for('reaction_add', timeout=secTimeout, check=check)
-#         return reaction
-#     except asyncio.TimeoutError:
-#         return "timeout"
-
-def deadEmbed(type):
-    if(type == "timeout"):
-        return discord.Embed(title="nice one", description="self.shit()")
-    if(type == "kill"):
-        return discord.Embed(title="adios", description="i will remember you hermano")
-
 # 
 # 
 #  END OF FUNCTIONS, COMMANDS BELOW
 # 
 # 
-
-def getSchedule(ctx):
-
-    #Creates and Runs API Request [Roster Details]
-    base_url = 'https://statsapi.web.nhl.com/api/v1/schedule'
-    #url = base_url.format(team)
-    response = requests.get(base_url)
-    print(response.status_code)
-    #print(response.text)  
-
-    #Makes JSON Information A Python Object
-    scheduleJSON = response.text 
-    scheduleData = json.loads(scheduleJSON)
-
-    totalGames = scheduleData['totalGames']
-    print(totalGames) 
-
-    #Create Embed 
-    myEmbed = discord.Embed(title = f" Schedule For: ", description = f" Today! \n", color = 0x00ff00)            
-    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url) 
-    myEmbed.set_footer(text = "NHL BOT -- CREATED BY: SHUSHAWN & SHAILEN") 
-    myEmbed.set_thumbnail(url=f"https://www-league.nhlstatic.com/images/logos/league-dark/133-flat.svg")
-
-    #Adds Details For All Games
-    x = 0
-    for x in range(totalGames):
-        myEmbed.add_field(name = f"** {scheduleData['dates'][0]['games'][x]['teams']['away']['team']['name']} vs. {scheduleData['dates'][0]['games'][x]['teams']['home']['team']['name']} **", value = f" > Arena: {scheduleData['dates'][0]['games'][x]['venue']['name']} \n > ", inline = True)
-
-
-    return myEmbed 
 
 #Handles '!' commands
 @client.command(name='nhl')
@@ -254,19 +64,19 @@ async def _nhl(ctx, *args):
         author = discord.User.id       
         
         if args[0] == 'roster':
-            rosterEmbed = getRoster(ctx, args[1]) 
+            rosterEmbed = functions.getRoster(ctx, args[1]) 
             await ctx.send(embed = rosterEmbed)
              
         elif args[0] == 'player': 
             if (args[2]):
                 name = args[1] + " " + args[2]
-            id = getPlayerID(ctx, name)
-            playerEmbed = getPlayer(ctx, id)
+            id = functions.getPlayerID(ctx, name)
+            playerEmbed = functions.getPlayer(ctx, id)
             
             await ctx.send(embed = playerEmbed) 
         
         elif args[0] == 'schedule': 
-            scheduleEmbed = getSchedule(ctx)
+            scheduleEmbed = functions.getSchedule(ctx)
             await ctx.send(embed = scheduleEmbed)
         
         elif args[0] == 'teamstats': 
@@ -276,42 +86,13 @@ async def _nhl(ctx, *args):
             maxTeamID = 30
             brokenIDs = [11,27] 
 
-            def statsEmbed(teamID):
-
-                #Creates and Runs API Request [Team Stats Details]
-                #print(f"GETTING TEAMID DATA {teamID} !!!!!\n\n\n")
-                base_url = 'https://statsapi.web.nhl.com/api/v1/teams/{}/stats'
-                url = base_url.format(teamID)
-                response = requests.get(url)
-                #print(response.status_code)
-                #print(response.text)
-
-                #Makes JSON Information A Python Object
-                nhlJSON = response.text
-                nhlData = json.loads(nhlJSON)
-
-                #Create Embed
-                try:
-                    myEmbed = discord.Embed(title=nhlData['stats'][0]['splits'][0]['team']['name'], description="")
-                    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url)
-                    myEmbed.set_footer(text = f"NHL Stats - Team ID: {teamID}")
-                    myEmbed.add_field(name="teamname", value=nhlData["stats"][0]["splits"][0]["team"]["name"], inline = True)
-                except:
-                    myEmbed = discord.Embed(title = "Error", description="Something went wrong. This team ID probably doesn't exist or doesn't have any data associated with it.")
-                    myEmbed.set_author(name=ctx.author.display_name, url="https://www.nhl.com/", icon_url=ctx.author.avatar_url)
-                    myEmbed.set_footer(text = f"NHL Stats - Team ID: {teamID}")
-                    #myEmbed.add_field(name="?????", value="This team ID probably doesn't exist.", inline = True)
-
-                return myEmbed
-
-            teamEmbed = await ctx.send(embed = statsEmbed(teamID))
+            teamEmbed = await ctx.send(embed = functions.getTeamEmbed(ctx,teamID))
 
             await teamEmbed.add_reaction("⬅️")
             await teamEmbed.add_reaction("➡️")
 
             def check(reaction, user):
                 return user == ctx.message.author and reaction.message == teamEmbed and (reaction.emoji == "⬅️" or reaction.emoji == "➡️")
-
 
             wrongNavs = 0
 
@@ -340,14 +121,14 @@ async def _nhl(ctx, *args):
                     if(reaction.emoji == "⬅️"):
                         if teamID > 1:
                             teamID -= 1
-                            await teamEmbed.edit(embed = statsEmbed(teamID))
+                            await teamEmbed.edit(embed = functions.getTeamEmbed(ctx,teamID))
                         else:
                             wrongNavs+=1
                             await reaction.remove(user)
                     elif(reaction.emoji == "➡️"):
                         if teamID < maxTeamID:
                             teamID += 1
-                            await teamEmbed.edit(embed = statsEmbed(teamID))
+                            await teamEmbed.edit(embed = functions.getTeamEmbed(ctx,teamID))
                         else:
                             wrongNavs +=1
                             await reaction.remove(user)
@@ -373,7 +154,7 @@ async def _nhl(ctx, *args):
 async def _roster(ctx:SlashContext, team):
     #print(f"THIS IS CRAZY: {team}")
     #await ctx.send(f"{team}")  
-    rosterEmbed = getRoster(ctx, team) 
+    rosterEmbed = functions.getRoster(ctx, team) 
     await ctx.send(embed = rosterEmbed) 
 
 #[player] slash command
@@ -400,8 +181,8 @@ async def _player(ctx:SlashContext, name):
     #print(f"THIS IS CRAZY: {team}")
     #await ctx.send(f"{team}") 
 
-    id = getPlayerID(ctx, name) 
-    playerEmbed = getPlayer(ctx, id)
+    id = functions.getPlayerID(ctx, name) 
+    playerEmbed = functions.getPlayer(ctx, id)
     await ctx.send(embed = playerEmbed)   
 
 # Run the client on discord server
